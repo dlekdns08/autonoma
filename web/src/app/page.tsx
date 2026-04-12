3f03a1b1bbe74eb97bd9fe9a762076a190ac905d
idaun <dlekdns08@gmail.com>
2026-04-12 12:53:27 +0900

    feat: 인증 상태에 따른 AuthModal 컴포넌트 추가

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -78,6 +78,9 @@ export default function Home() {
           />
         </main>
         <ToastContainer toasts={toasts} onDismiss={dismissToast} />
+        {needsAuth && authState.status !== "unknown" && (
+          <AuthModal authState={authState} onAuthenticate={authenticate} />
+        )}
       </div>
     );
   }
